e2c8e823b714667c6b61fc8cd95d6e41e9a610ae
Shiaroku <sbmeyer68@gmail.com>
2023-07-03 11:10:29 +0200

    Update keymaps from NERDTree to Telescope

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,7 +10,10 @@ local opts = {
 -- Commands
 vim.cmd('cnoreabbrev init luafile $MYVIMRC') -- remap ':init' to refresh the config file
 
-vim.keymap.set('n', '<Leader>n', ':NERDTreeFocus<CR>', opts)
-vim.keymap.set('n', '<C-n>', ':NERDTree<CR>', opts)
-vim.keymap.set('n', '<C-t>', ':NERDTreeToggle<CR>', opts)
-vim.keymap.set('n', '<C-f>', ':NERDTreeFind<CR>', opts)
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
